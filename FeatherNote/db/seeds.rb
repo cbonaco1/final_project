13020a727120c55c86195a6709eb7c9ec0885d74
App Academy Student <appacademy@Apps-Mac-mini-53.local>
2016-01-26 17:33:20 -0500

    Add seed data, journal, and more styling.

--- FeatherNote/db/seeds.rb
+++ FeatherNote/db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.destroy_all
+guest_user = User.new(username: "guest")
+guest_user.password = "guestpassword"
+guest_user.reset_session_token!
+
+user1 = User.new(username: "christian")
+user1.password = "password"
+user1.reset_session_token!
+
+user2 = User.new(username: "yoda")
+user2.password = "password"
+user2.reset_session_token!
+
+user3 = User.new(username: "johnsmith")
+user3.password = "password"
+user3.reset_session_token!

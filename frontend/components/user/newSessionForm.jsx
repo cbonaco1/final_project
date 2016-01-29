dd95d4f865dd23d1c17ba8364b2bcd96165bdc83
cbonaco1 <christian.bonacore@gmail.com>
2016-01-28 20:32:41 -0500

    Made new session and new user React forms.

--- frontend/components/user/newSessionForm.jsx
+++ frontend/components/user/newSessionForm.jsx
@@ -1,9 +1,38 @@
 var React = require('react');
 
 var NewSessionForm = React.createClass({
+
+  submit: function() {
+
+  },
+
   render: function() {
     return(
-      <p>New Session form!</p>
+      <div className="user-form">
+        <h2 className="form-header">Sign in</h2>
+        <form className="new-form" onSubmit={this.submit}>
+
+          <div className="input-field">
+            <label>Username:
+              <input type="text" name="user[username]" autofocus />
+            </label>
+          </div>
+
+          <div className="input-field">
+            <label>Password:
+              <input type="password" name="user[password]" />
+            </label>
+          </div>
+
+          <button className="form-button">Sign In</button>
+        </form>
+
+        <div className="sign-in-msg">
+          <p>Dont have an account?</p>
+          <a href="#/users/new">Create account</a>
+        </div>
+
+      </div>
     );
   }
 });
--- frontend/components/user/newUserForm.jsx
+++ frontend/components/user/newUserForm.jsx
@@ -3,7 +3,32 @@ var React = require('react');
 var NewUserForm = React.createClass({
   render: function() {
     return(
-      <p>New user form!</p>
+      <div className="user-form">
+        <h2 className="form-header">Create Account</h2>
+        <form className="new-form" action="<%= users_url %>" method="post">
+
+          <div className="input-field">
+            <label>Username:
+              <input type="text" name="user[username]" autofocus />
+            </label>
+          </div>
+
+          <div className="input-field">
+            <label>Password:
+              <input type="password" name="user[password]" />
+            </label>
+          </div>
+
+          <button className="form-button">Create Account!</button>
+
+        </form>
+
+        <div className="sign-in-msg">
+          <p>Already have an account?</p>
+          <a href="#/session/new">Sign in</a>
+        </div>
+
+      </div>
     );
   }
 });
